70e07a4de0a3c16bb6c4b13829ade720af5b6193
ieshaan12 <ieshaan1999@gmail.com>
2020-09-05 18:28:04 +0530

    Updated

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 with open("README.md", "r") as fileObj:
   longDescription = fileObj.read()
 
-setuptools.setup(name="PyProfane-v1.0.0",
+setuptools.setup(name="PyProfane",
   packages=setuptools.find_packages(),
-  version="1.0.0",
+  version="1.0.1",
   author="Ieshaan Saxena",
   author_email="ieshaan1999@gmail.com",
   description="A library to censor and detect offensive words in strings.",
@@ -13,7 +13,7 @@ setuptools.setup(name="PyProfane-v1.0.0",
   long_description_content_type="text/markdown",
   license="MIT",
   url="https://github.com/ieshaan12/PyProfane",
-  download_url="https://github.com/ieshaan12/PyProfane/archive/v1.0.1.tar.gz",
+  download_url="https://github.com/ieshaan12/PyProfane/archive/PyProfane-v1.0.1.tar.gz",
   package_data={'PyProfane': ['data/swearWords.txt', 'data/comments.txt']},
   classifiers=[
     "Development Status :: 3 - Alpha",
